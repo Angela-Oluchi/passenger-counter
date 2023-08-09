8fa43a6a7f1f3d2b73a1bf2d6ff96e40970efd2e
Angela Ria <angelaabugu01@gmail.com>
2023-08-09 10:54:00 +0100

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 let count = 0;
 
-let incrementEl = document.getElementById("count")
+let incrementEl = document.getElementById("count")  // where the num is increasing
+let entriesEl = document.getElementById("entries-el") // the previous entries saved
+
 
 function increment() {
  incrementEl.textContent = count += 1 
 }
 
-let entriesEl = document.getElementById("entries-el")
-
-// console.log(entriesEl)
 function save(){
-    let addition = count + "-"
-   entriesEl.textContent += addition
+    let saving = count + "-"
+   entriesEl.textContent += saving
    incrementEl.textContent = 0
    count = 0
+}
+
+function decrease(){
+    incrementEl.textContent = count -= 1 
 }
